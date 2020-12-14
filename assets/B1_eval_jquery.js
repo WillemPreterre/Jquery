ef7391cb7e6da6333a554d18ec9f9e10ded0e3af
Willem <wpreterre.lamanu@gmail.com>
2020-12-14 12:29:48 +0100

    Eval sauvegarde

--- assets/B1_eval_jquery.js
+++ assets/B1_eval_jquery.js
@@ -1,5 +1,47 @@
-/* --------------------------------------------------------------------------
 
-Votre code ici
 
--------------------------------------------------------------------------- */
+function exercice1() {
+
+
+    $('.custom-control-label').click(function (e) { 
+        e.preventDefault();
+        
+    $('.redsquare-app').removeClass('is-active');
+    $(this).addClass('is-active');
+
+         
+    $('.redsquare-controls').removeAttr('disabled');
+    $(this).attr('disabled');
+    });
+}
+
+function exercice2() {
+
+}
+
+
+function exercice3() {
+    if ($('.redsquare-controls').hasClass('is-active'));
+    $('#scaleUp').click(function (e) { 
+        e.preventDefault();
+        $('.square').css('widht')
+    })
+}
+
+function jumbotron() {
+
+    $('.jumbotron a').click(function(event){
+        event.preventDefault();
+    });
+
+    $('.jumbotron ul').addClass('list-group');
+    $('.jumbotron il').addClass('list-group-item');
+}
+
+$(function () {
+    console.log('Document chargé')
+    exercice1()
+    exercice2()
+    exercice3()
+    jumbotron()
+})  
